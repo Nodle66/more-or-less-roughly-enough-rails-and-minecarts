2558db9fb60fb8d80bb4d09b3664dbd3e4613e96
Delibasa Dejan <nodle66@gmail.com>
2024-02-10 20:54:46 +0200

    Initial commit

---
+++ src/main/java/nodle66/moreorlessroughlyenoughrails/MoreorlessroughlyenoughrailsDataGenerator.java
@@ -0,0 +1,11 @@
+package nodle66.moreorlessroughlyenoughrails;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MoreorlessroughlyenoughrailsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
