c252c2e7953f7d28b9ec3940a0e80230a3042fc8
Nodle66 <nodle66@gmail.com>
2024-02-12 12:15:08 +0200

    Added method to add items to redstone item group for posterity

--- src/main/java/nodle66/moreorlessroughlyenoughrails/items/ModItems.java
+++ src/main/java/nodle66/moreorlessroughlyenoughrails/items/ModItems.java
@@ -1,6 +1,10 @@
 package nodle66.moreorlessroughlyenoughrails.items;
 
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
@@ -14,7 +18,11 @@ public class ModItems {
                 new Identifier(Moreorlessroughlyenoughrails.getMOD_ID(), name),
                 item);
     }
+    private static void addItemsToRedstoneItemGroup(FabricItemGroupEntries entries) {
+
+    }
     public static void registerModItems() {
         Moreorlessroughlyenoughrails.Logger("Registering mod items for " + Moreorlessroughlyenoughrails.getMOD_ID() + "!");
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.REDSTONE).register(ModItems::addItemsToRedstoneItemGroup);
     }
 }
