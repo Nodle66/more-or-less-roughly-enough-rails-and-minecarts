bed408999e14d94a8947244045db7e7df9b4f22a
Nodle66 <nodle66@gmail.com>
2024-02-22 09:07:20 +0200

    Still don't get it

--- src/main/java/nodle66/moreorlessroughlyenoughrails/entity/custom/minecart/EnderChestMinecartEntity.java
+++ src/main/java/nodle66/moreorlessroughlyenoughrails/entity/custom/minecart/EnderChestMinecartEntity.java
@@ -7,15 +7,25 @@ import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.vehicle.MinecartEntity;
 import net.minecraft.entity.vehicle.VehicleEntity;
+import net.minecraft.block.EnderChestBlock;
+import net.minecraft.inventory.EnderChestInventory;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.screen.GenericContainerScreenHandler;
+import net.minecraft.screen.SimpleNamedScreenHandlerFactory;
+import net.minecraft.stat.Stats;
 import net.minecraft.text.Text;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
 import nodle66.moreorlessroughlyenoughrails.items.ModItems;
 
+import javax.swing.*;
+
 public class EnderChestMinecartEntity extends MinecartEntity {
 
-
+    private static final Text CONTAINER_NAME = Text.translatable("container.moreorlessroughlyenoughrails.ender_chest_minecart");
     public EnderChestMinecartEntity(EntityType<? extends MinecartEntity> entityType, World world) {
         super(entityType, world);
     }
@@ -26,23 +36,19 @@ public class EnderChestMinecartEntity extends MinecartEntity {
     }
     @Override
     public BlockState getDefaultContainedBlock() {
-        return Blocks.ENDER_CHEST.getDefaultState();
+        return (BlockState) Blocks.ENDER_CHEST.getDefaultState().with(EnderChestBlock.FACING, Direction.NORTH);
     }
 
     @Override
     public ItemStack getPickBlockStack() {
         return new ItemStack(ModItems.ENDER_CHEST_MINECART, 1);
     }
-    public void open() {
-        PlayerEntity debugPlayer = getWorld().getClosestPlayer(this.getX(), this.getY(), this.getZ(), 32, false);
-        if(debugPlayer != null) {
-            debugPlayer.sendMessage(Text.literal("open()"));
-        }
-    }
-    public void onUse() {
-        PlayerEntity debugPlayer = getWorld().getClosestPlayer(this.getX(), this.getY(), this.getZ(), 32, false);
-        if(debugPlayer != null) {
-            debugPlayer.sendMessage(Text.literal("onUse()"));
-        }
+
+    @Override
+    public ActionResult interact (PlayerEntity player, Hand hand) {
+        EnderChestInventory enderChestInventory = player.getEnderChestInventory();
+        player.openHandledScreen(new SimpleNamedScreenHandlerFactory((syncId, inventory, playerx) -> GenericContainerScreenHandler.createGeneric9x3(syncId, inventory, enderChestInventory), CONTAINER_NAME));
+        player.incrementStat(Stats.OPEN_ENDERCHEST);
+        return ActionResult.CONSUME;
     }
 }
